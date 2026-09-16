214061816276e305fda7cc32859d2d5c8aed7b78
ISINNA473 <jin031655@gmail.com>
2026-09-16 17:56:16 +0900

    .

---
+++ Assets/VariableExample.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class VariableExample : MonoBehaviour
+{
+    int a;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        a = 1;
+        Debug.Log(a);
+        //Debug.Log("Hellow World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
